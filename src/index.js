35822629b3b0bf01369ca236528de2074e8add4f
luochen <lc@bcxs.com>
2020-09-28 18:52:09 +0800

    修改暴露的接口,未完成

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 
-import Node from './elements/node.js'
+import View from './elements/node.js'
 import Link from './elements/link.js'
 import Image from './elements/image.js'
 import Video from './elements/video.js'
 import Input from './elements/input.js'
+import Text from './elements/text.js'
 
 import render from './render/index'
 
-class Ezdo {
+class Aut {
     constructor() {
         this.type = 'Body'
-        this.Node = Node
+        this.Node = View
         this.Link = Link
         this.Image = Image
         this.Video = Video
@@ -39,6 +40,35 @@ class Ezdo {
 
         this[module.name] = module
     }
+
+    // 添加一个扩展组件的接口
+    // installPlugin(pulu) {
+
+    // }
+
+    // extendComponents(ui) {
+    //     if (ui && ui.init) {
+    //         ui.init()
+    //     }
+    // }
 }
 
-export default new Ezdo
+export default new Aut
+
+// Base DisplayObject
+// export const View = Node
+// export const Image = Image1
+// export const Text = Text
+// export const Video = Video
+// export const Audio = Audio
+
+export {
+    View,
+    Image,
+    Video,
+    // Audio,
+    Text
+}
+
+// 扩展
+
